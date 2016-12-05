100c1fc4d757cefa57402fcadb112a69434dca8e
yaoenqi <yaoenqi@163.com>
2016-12-05 14:27:55 +0800

    Create zhCN.lua

---
+++ ImpBlizzardUI/loc/zhCN.lua
@@ -0,0 +1,61 @@
+local _, ImpBlizz = ...;
+
+if(GetLocale() == "zhCN") then
+
+    -- Micro Menu
+    ImpBlizz["Character"] = "角色信息";
+    ImpBlizz["Spellbook"] = "法术书和技能";
+    ImpBlizz["Talents"] = "天赋和专精";
+    ImpBlizz["Achievements"] = "成就";
+    ImpBlizz["Quest Log"] = "任务日志";
+    ImpBlizz["Guild"] = "公会";
+    ImpBlizz["Group Finder"] = "地下城和团队副本";
+    ImpBlizz["Collections"] = "藏品";
+    ImpBlizz["Dungeon Journal"] = "冒险指南";
+    ImpBlizz["Swap Bags"] = "切换背包";
+    ImpBlizz["ImpBlizzardUI"] = "ImpBlizzardUI"
+    ImpBlizz["Log Out"] = "返回人物选择";
+    ImpBlizz["Force Exit"] = "立刻退出";
+
+    -- Merchant
+    ImpBlizz["Items Repaired from Guild Bank"] = "已使用公会修理";
+    ImpBlizz["Can not Repair from Guild Bank"] = "不能使用公会修理";
+    ImpBlizz["Items Repaired from Own Money"] = "已自费修理";
+    ImpBlizz["Sold Trash Items"] = "已出售垃圾";
+
+    -- Combat
+    ImpBlizz[" killed "] = " 被击杀 ";
+    ImpBlizz["Killing Blow!"] = "致命一击!";
+    ImpBlizz["HP < 50% !"] = "生命值低于50%!";
+    ImpBlizz["HP < 25% !!!"] = "生命值低于25%!!!";
+
+    -- Config Headers
+    ImpBlizz["Combat"] = "战斗";
+    ImpBlizz["Miscellaneous"] = "杂项";
+    ImpBlizz["User Interface"] = "用户界面";
+
+    -- Config Options
+    ImpBlizz["Display Class Icon"] = "显示职业图标";
+    ImpBlizz["Display Health Warnings"] = "显示生命值预警";
+    ImpBlizz["Display PvP Kill Tracker"] = "显示PVP击杀追踪";
+    ImpBlizz["Display Class Colours"] = "显示职业颜色";
+    ImpBlizz["Auto Repair"] = "自动修理";
+    ImpBlizz["Use Guild Bank For Repairs"] = "使用公会修理";
+    ImpBlizz["Auto Sell Trash"] = "自动出售垃圾";
+    ImpBlizz["AFK Mode"] = "暂离模式";
+    ImpBlizz["Display System Statistics"] = "显示系统统计";
+    ImpBlizz["Display Player Co-Ordinates"] = "显示玩家坐标";
+    ImpBlizz["Display Art"] = "显示动作条狮鹫";
+    ImpBlizz["Auto-Hide Quest Tracker"] = "自动隐藏任务追踪";
+    ImpBlizz["Highlight Killing Blows"] = "高亮致命一击";
+    ImpBlizz["PvP"] = "PVP";
+    ImpBlizz["Minify Blizzard Strings"] = "缩小暴雪字符串";
+    ImpBlizz["Style Chat"] = "聊天样式";
+    ImpBlizz["Hide Chat Arrows"] = "隐藏聊天箭头";
+    ImpBlizz["Action Bars"] = "动作条";
+    ImpBlizz["Casting Bar Timer"] = "施法条计时器";
+    ImpBlizz["Out of Range Indicator"] = "超出范围监视器";
+    ImpBlizz["Hide Portrait Spam"] = "隐藏头像战斗文字";
+    ImpBlizz["Style Chat Bubbles"] = "聊天泡泡样式";
+
+end
